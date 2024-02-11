154b441c802b0d2fbd46384f5db96b48e3bda649
Giedrius Išora <g.isora@outlook.com>
2024-02-11 13:37:47 +0200

    create page module

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -1,11 +1,15 @@
 #![allow(non_snake_case)]
 
 use dioxus::prelude::*;
+use dioxus_router::Router;
 use fermi::use_init_atom_root;
 
 pub fn App(cx: Scope) -> Element {
     use_init_atom_root(cx);
+    let other_things = rsx! { p {"paragraph test"}};
     cx.render(rsx!{
-        h1 {"hello, world!"}
+        Router {
+            Route { to: "some_url", page::Register {} },
+        }
     })
 }
